1ea733146a2fd70d8c0edf0a598dc2b728948b3e
Alfredo Ormeno <ormenoandres13@gmail.com>
2023-08-27 02:45:32 -0400

    Create the Knight class

---
+++ knight.rb
@@ -0,0 +1,10 @@
+class Knight
+  def initialize(position, parent = nil)
+    # Current location on the game board
+    @position = position
+    # A pointer to its origin (previous position)
+    @parent = parent
+    # Push postion to history array in game board class
+    Board.new.history.push(position)
+  end
+end 
